5f7ca7997b62ec63b728b71b780dd813f5b0b67b
yseonjin <yseonjin@gmail.com>
2021-06-19 20:56:21 +0900

    feat: 회원가입 이메일 중복확인하기

--- be/src/services/interface/AuthService.ts
+++ be/src/services/interface/AuthService.ts
@@ -3,5 +3,5 @@ export interface AuthService {
   getUser: (accessToken: string) => Promise<any>;
   getHashedPassword: (password: string) => Promise<string>;
   registerUser: (name: string, email: string, password: string) => Promise<any>;
-  // registerMember:
+  checkDupeUser: (email: string) => Promise<boolean>;
 }
