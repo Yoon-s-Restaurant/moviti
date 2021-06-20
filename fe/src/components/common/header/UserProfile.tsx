031955ce995caf0849a0314a92f7d0de8cc3eae3
pius712 <aksfbsgnlfls@gmail.com>
2021-06-20 17:53:32 +0900

    feat: display UserProfile name

--- fe/src/components/common/header/UserProfile.tsx
+++ fe/src/components/common/header/UserProfile.tsx
@@ -6,7 +6,7 @@ interface UserProfileProps {
   userData: UserData;
 }
 const UserProfile = ({ userData }: UserProfileProps) => {
-  return <UserName>{userData.email}</UserName>;
+  return <UserName>{userData.name}</UserName>;
 };
 
 export default UserProfile;
